a45bf72610ebe790b593dca6a1ccca293df67956
markdiscbreak <markdiscbreak@gmail.com>
2026-03-29 18:49:27 +0100

    cloudinary new

--- netlify/functions/cloudinary.js
+++ netlify/functions/cloudinary.js
@@ -1,26 +1,34 @@
 exports.handler = async function() {
-  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
-  const apiKey = process.env.CLOUDINARY_API_KEY;
-  const apiSecret = process.env.CLOUDINARY_API_SECRET;
+  try {
+    const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+    const apiKey = process.env.CLOUDINARY_API_KEY;
+    const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
-  const credentials = Buffer.from(`${apiKey}:${apiSecret}`).toString('base64');
+    const credentials = Buffer.from(`${apiKey}:${apiSecret}`).toString('base64');
 
-  const res = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/resources/image?max_results=100`, {
-    headers: {
-      'Authorization': `Basic ${credentials}`
-    }
-  });
+    const res = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/resources/image?max_results=100`, {
+      headers: {
+        'Authorization': `Basic ${credentials}`
+      }
+    });
 
-  const data = await res.json();
-  
-  const images = data.resources.map(img => ({
-    filename: img.public_id,
-    url: `https://res.cloudinary.com/${cloudName}/image/upload/${img.public_id}.${img.format}`
-  }));
+    const data = await res.json();
 
-  return {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify(images)
+    const images = data.resources.map(img => ({
+      filename: img.public_id,
+      url: `https://res.cloudinary.com/${cloudName}/image/upload/${img.public_id}.${img.format}`
+    }));
+
+    return {
+      statusCode: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify(images)
+    };
+
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
   }
 }
